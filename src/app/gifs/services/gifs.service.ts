aeab8f7b83b49f6b239e8c8eb419bc7b3475e571
Maewa Rodríguez <maewa30@gmail.com>
2021-04-22 19:01:12 +0200

    actualizado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'UJgqPFhSMmuuWnqVfov4eciJRxY5dgYU';
+
   private _historial:string[] = [];
 
   get historial(){
@@ -23,6 +25,15 @@ export class GifsService {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
     }
+
+    //TODO 
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=uN2kmlBTlGk4KEMWmmU4n5gspqeElUh6=dragon')
+    .then(resp => {
+      resp.json().then(data =>{
+        console.log(data)
+      })
+    })
+
 
     console.log(this._historial)
   }
